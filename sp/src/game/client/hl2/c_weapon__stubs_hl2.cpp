d181e9bb4d652d8cf2acd517d5d8539761bb2022
Iliketocreatestuff2001 <Zombiegoon67@gmail.com>
2025-03-18 18:53:07 +0000

    Update c_weapon__stubs_hl2.cpp

--- sp/src/game/client/hl2/c_weapon__stubs_hl2.cpp
+++ sp/src/game/client/hl2/c_weapon__stubs_hl2.cpp
@@ -23,6 +23,9 @@ STUB_WEAPON_CLASS( weapon_cubemap, WeaponCubemap, C_BaseCombatWeapon );
 STUB_WEAPON_CLASS( weapon_alyxgun, WeaponAlyxGun, C_HLSelectFireMachineGun );
 STUB_WEAPON_CLASS( weapon_citizenpackage, WeaponCitizenPackage, C_BaseHLCombatWeapon );
 STUB_WEAPON_CLASS( weapon_citizensuitcase, WeaponCitizenSuitcase, C_WeaponCitizenPackage );
+STUB_WEAPON_CLASS(weapon_glock18, WeaponGlock18, C_BaseHLCombatWeapon);
+STUB_WEAPON_CLASS(weapon_mp5, WeaponMP5, C_HLSelectFireMachineGun);
+//STUB_WEAPON_CLASS(weapon_sniperrifle, WeaponSniperRifle, C_HLSelectFireMachineGun); // TODO: Maybe add a sniper rifle?
 
 #ifndef HL2MP
 STUB_WEAPON_CLASS( weapon_ar2, WeaponAR2, C_HLMachineGun );
