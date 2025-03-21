1c364e5c0eed99f56f24cb3119a77ec5f967e27c
Iliketocreatestuff2001 <Zombiegoon67@gmail.com>
2025-03-21 20:36:25 +0000

    Update logic_assault.h

--- sp/src/game/server/logic_assault.h
+++ sp/src/game/server/logic_assault.h
@@ -6,9 +6,10 @@
 class CLogicAssault : public CLogicalEntity
 {
 	DECLARE_CLASS(CLogicAssault, CLogicalEntity);
-	DECLARE_DATADESC();
 public:
+	CLogicAssault();
 	void Spawn(void);
+	void Precache(void);
 	void AssaultThink(void);
 	void SUB_DoNothing(void) {};
 	bool HumanHullFits(const Vector& vecLocation);
@@ -21,13 +22,14 @@ public:
 	void ChildPostSpawn(CAI_BaseNPC* pChild);
 	const char* GetEnemyType();
 	virtual bool KeyValue(const char* szKeyName, const char* szValue);
+
+	DECLARE_DATADESC();
 private:
 	int m_iMaxEnemies;
-	int m_iMaxEnemiesPerWave;
 
 	bool m_bDisabled;
 	bool m_bEndlessWaves; // if enabled, the m_iMaxWaves value is ignored and waves go on and on
-	int m_iNumEnemies; // max enemies in a wave
+	int m_iNumEnemies; // num enemies in a wave to keep track of
 	int m_iNumWave; // the wave number to keep track of, when spawning enemy squads
 	int m_iMaxWaves; // the maximum number of waves the assault will have
 	int m_iPhase; // the phase of this wave, each wave has three phases before proceeding to the next wave and resetting this value
@@ -40,7 +42,8 @@ private:
 	float m_fGrenadeChance;
 	int m_iGrenadeCount;
 
-	float m_flSpawnFrequency;
+	float m_flSpawnFrequency; // how often spawn attempts occur
+	float m_flTimeUntilNextWave; // time until next wave
 
 	float m_fEnemyCitizenMedicChance;
 
@@ -50,6 +53,7 @@ private:
 	float m_fSpawnDistance;
 
 	bool m_bShouldUpdateEnemies;
+	bool m_bIsAssaultActivated;
 
 	void InputEnable(inputdata_t& inputdata);
 	void InputDisable(inputdata_t& inputdata);
@@ -58,6 +62,7 @@ private:
 	COutputEHANDLE m_OnSpawnNPC; // Fired when the wave spawns an NPC (!activator is the NPC)
 	COutputInt m_OnNextWave; // Fired when the next wave arrives
 	COutputEvent m_OnAllWavesDefeated; // Fired when all waves are defeated (only if endless waves isn't set)
+	COutputEvent m_OnAssaultStart; // Fired when the assault starts (happens once per entity)
 };
 
 #endif
