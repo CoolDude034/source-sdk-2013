4348f87fb89972d24cd6a1f1107cd9669bc450c6
Iliketocreatestuff2001 <Zombiegoon67@gmail.com>
2025-03-22 17:46:21 +0000

    Create weapon_sniperrifle.cpp

---
+++ sp/src/game/server/weapon_sniperrifle.cpp
@@ -0,0 +1,351 @@
+//========= Copyright Valve Corporation, All rights reserved. ============//
+//
+// Purpose: Sniper Rifle
+//
+//=============================================================================//
+
+#include "cbase.h"
+#include "basehlcombatweapon.h"
+#include "npcevent.h"
+#include "basecombatcharacter.h"
+#include "ai_basenpc.h"
+#include "player.h"
+#include "game.h"
+#include "in_buttons.h"
+#include "ai_memory.h"
+#include "soundent.h"
+#include "rumble_shared.h"
+#include "gamestats.h"
+#include "actual_bullet.h"
+
+// memdbgon must be the last include file in a .cpp file!!!
+#include "tier0/memdbgon.h"
+
+extern ConVar sv_enable_hitscan_weapons;
+extern ConVar sk_bullet_speed;
+
+//-----------------------------------------------------------------------------
+// CWeaponSniperRifle
+//-----------------------------------------------------------------------------
+class CWeaponSniperRifle : public CHLSelectFireMachineGun
+{
+	DECLARE_DATADESC();
+
+public:
+	DECLARE_CLASS(CWeaponSniperRifle, CHLSelectFireMachineGun);
+
+	CWeaponSniperRifle();
+
+	DECLARE_SERVERCLASS();
+
+	void			Precache();
+	void			AddViewKick();
+	void			SecondaryAttack();
+
+	int				GetMinBurst() { return 1; }
+	int				GetMaxBurst() { return 1; }
+
+	virtual void	Equip(CBaseCombatCharacter* pOwner);
+	bool			Reload();
+
+	float			GetFireRate() { return 0.075f; } // RPS, 60sec/800 rounds = 0.075
+	int				CapabilitiesGet() { return bits_CAP_WEAPON_RANGE_ATTACK1; }
+	int				WeaponRangeAttack2Condition(float flDot, float flDist);
+	Activity		GetPrimaryAttackActivity();
+
+	virtual bool	HasIronsights(void) { return false; } // sniper rifle has custom scope
+
+	// Values which allow our "spread" to change from button input from player
+	virtual const Vector& GetBulletSpread()
+	{
+		// Define "spread" parameters based on the "owner" and what they are doing
+		static Vector aimingCone = VECTOR_CONE_1DEGREES;
+		static Vector plrDuckCone = VECTOR_CONE_2DEGREES;
+		static Vector plrStandCone = VECTOR_CONE_3DEGREES;
+		static Vector plrMoveCone = VECTOR_CONE_4DEGREES;
+		static Vector npcCone = VECTOR_CONE_5DEGREES;
+
+		if (GetOwner() && GetOwner()->IsNPC())
+			return npcCone;
+
+		if (m_bIsIronsighted)
+		{
+			return aimingCone;
+		}
+
+		// We must know the player "owns" the weapon before different cones may be used
+		CBasePlayer* pPlayer = ToBasePlayer(GetOwnerEntity());
+		if (pPlayer->m_nButtons & IN_DUCK)
+			return plrDuckCone;
+		if (pPlayer->m_nButtons & IN_FORWARD)
+			return plrMoveCone;
+		if (pPlayer->m_nButtons & IN_BACK)
+			return plrMoveCone;
+		if (pPlayer->m_nButtons & IN_MOVERIGHT)
+			return plrMoveCone;
+		if (pPlayer->m_nButtons & IN_MOVELEFT)
+			return plrMoveCone;
+		if (pPlayer->m_nButtons & IN_SPEED)
+			return plrMoveCone;
+		if (pPlayer->m_nButtons & IN_RUN)
+			return plrMoveCone;
+		else
+			return plrStandCone;
+	}
+
+	const WeaponProficiencyInfo_t* GetProficiencyValues();
+
+	void FireNPCPrimaryAttack(CBaseCombatCharacter* pOperator, Vector& vecShootOrigin, Vector& vecShootDir);
+	void Operator_ForceNPCFire(CBaseCombatCharacter* pOperator, bool bSecondary);
+	void Operator_HandleAnimEvent(animevent_t* pEvent, CBaseCombatCharacter* pOperator);
+
+	DECLARE_ACTTABLE();
+};
+
+IMPLEMENT_SERVERCLASS_ST(CWeaponSniperRifle, DT_WeaponSniperRifle)
+END_SEND_TABLE()
+
+LINK_ENTITY_TO_CLASS(weapon_sniperrifle, CWeaponSniperRifle);
+PRECACHE_WEAPON_REGISTER(CWeaponSniperRifle);
+
+BEGIN_DATADESC(CWeaponSniperRifle)
+END_DATADESC()
+
+acttable_t CWeaponSniperRifle::m_acttable[] =
+{
+	{ ACT_RANGE_ATTACK1,			ACT_RANGE_ATTACK_SMG1,			true },
+	{ ACT_RELOAD,					ACT_RELOAD_SMG1,				true },
+	{ ACT_IDLE,						ACT_IDLE_SMG1,					true },
+	{ ACT_IDLE_ANGRY,				ACT_IDLE_ANGRY_SMG1,			true },
+
+	{ ACT_WALK,						ACT_WALK_RIFLE,					true },
+	{ ACT_WALK_AIM,					ACT_WALK_AIM_RIFLE,				true  },
+
+	// Readiness activities (not aiming)
+	{ ACT_IDLE_RELAXED,				ACT_IDLE_SMG1_RELAXED,			false }, // Never aims
+	{ ACT_IDLE_STIMULATED,			ACT_IDLE_SMG1_STIMULATED,		false },
+	{ ACT_IDLE_AGITATED,			ACT_IDLE_ANGRY_SMG1,			false }, // Always aims
+
+	{ ACT_WALK_RELAXED,				ACT_WALK_RIFLE_RELAXED,			false }, // Never aims
+	{ ACT_WALK_STIMULATED,			ACT_WALK_RIFLE_STIMULATED,		false },
+	{ ACT_WALK_AGITATED,			ACT_WALK_AIM_RIFLE,				false }, // Always aims
+
+	{ ACT_RUN_RELAXED,				ACT_RUN_RIFLE_RELAXED,			false }, // Never aims
+	{ ACT_RUN_STIMULATED,			ACT_RUN_RIFLE_STIMULATED,		false },
+	{ ACT_RUN_AGITATED,				ACT_RUN_AIM_RIFLE,				false }, // Always aims
+
+	// Readiness activities (aiming)
+	{ ACT_IDLE_AIM_RELAXED,			ACT_IDLE_SMG1_RELAXED,			false }, // Never aims
+	{ ACT_IDLE_AIM_STIMULATED,		ACT_IDLE_AIM_RIFLE_STIMULATED,	false },
+	{ ACT_IDLE_AIM_AGITATED,		ACT_IDLE_ANGRY_SMG1,			false }, // Always aims
+
+	{ ACT_WALK_AIM_RELAXED,			ACT_WALK_RIFLE_RELAXED,			false }, // Never aims
+	{ ACT_WALK_AIM_STIMULATED,		ACT_WALK_AIM_RIFLE_STIMULATED,	false },
+	{ ACT_WALK_AIM_AGITATED,		ACT_WALK_AIM_RIFLE,				false }, // Always aims
+
+	{ ACT_RUN_AIM_RELAXED,			ACT_RUN_RIFLE_RELAXED,			false }, // Never aims
+	{ ACT_RUN_AIM_STIMULATED,		ACT_RUN_AIM_RIFLE_STIMULATED,	false },
+	{ ACT_RUN_AIM_AGITATED,			ACT_RUN_AIM_RIFLE,				false }, // Always aims
+	// End readiness activities
+
+	{ ACT_WALK_AIM,					ACT_WALK_AIM_RIFLE,				true },
+	{ ACT_WALK_CROUCH,				ACT_WALK_CROUCH_RIFLE,			true },
+	{ ACT_WALK_CROUCH_AIM,			ACT_WALK_CROUCH_AIM_RIFLE,		true },
+	{ ACT_RUN,						ACT_RUN_RIFLE,					true },
+	{ ACT_RUN_AIM,					ACT_RUN_AIM_RIFLE,				true },
+	{ ACT_RUN_CROUCH,				ACT_RUN_CROUCH_RIFLE,			true },
+	{ ACT_RUN_CROUCH_AIM,			ACT_RUN_CROUCH_AIM_RIFLE,		true },
+	{ ACT_GESTURE_RANGE_ATTACK1,	ACT_GESTURE_RANGE_ATTACK_SMG1,	true },
+	{ ACT_RANGE_ATTACK1_LOW,		ACT_RANGE_ATTACK_SMG1_LOW,		true },
+	{ ACT_COVER_LOW,				ACT_COVER_SMG1_LOW,				false },
+	{ ACT_RANGE_AIM_LOW,			ACT_RANGE_AIM_SMG1_LOW,			false },
+	{ ACT_RELOAD_LOW,				ACT_RELOAD_SMG1_LOW,			false },
+	{ ACT_GESTURE_RELOAD,			ACT_GESTURE_RELOAD_SMG1,		true },
+};
+
+IMPLEMENT_ACTTABLE(CWeaponSniperRifle);
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+//-----------------------------------------------------------------------------
+CWeaponSniperRifle::CWeaponSniperRifle()
+{
+	m_fMinRange1 = 0; // No minimum range
+	m_fMaxRange1 = 9500;
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+//-----------------------------------------------------------------------------
+void CWeaponSniperRifle::Precache()
+{
+	BaseClass::Precache();
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+//-----------------------------------------------------------------------------
+void CWeaponSniperRifle::Equip(CBaseCombatCharacter* pOwner)
+{
+	BaseClass::Equip(pOwner);
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+//-----------------------------------------------------------------------------
+void CWeaponSniperRifle::FireNPCPrimaryAttack(CBaseCombatCharacter* pOperator, Vector& vecShootOrigin, Vector& vecShootDir)
+{
+	// FIXME: Use the returned number of bullets to account for >10hz firerate
+	WeaponSoundRealtime(SINGLE_NPC);
+
+	CSoundEnt::InsertSound(SOUND_COMBAT | SOUND_CONTEXT_GUNFIRE, pOperator->GetAbsOrigin(), SOUNDENT_VOLUME_MACHINEGUN, 0.2, pOperator, SOUNDENT_CHANNEL_WEAPON, pOperator->GetEnemy());
+	if (sv_enable_hitscan_weapons.GetBool())
+	{
+		pOperator->FireBullets(1, vecShootOrigin, vecShootDir, VECTOR_CONE_PRECALCULATED, MAX_TRACE_LENGTH, m_iPrimaryAmmoType, 2, entindex(), 0);
+	}
+	else
+	{
+		FireBulletsInfo_t info;
+		info.m_iAmmoType = m_iPrimaryAmmoType;
+		info.m_iShots = 1;
+		info.m_vecSrc = vecShootOrigin;
+		info.m_vecDirShooting = vecShootDir;
+		info.m_vecSpread = GetBulletSpread();
+		info.m_pAttacker = GetOwnerEntity();
+
+		FireActualBullet(info, sk_bullet_speed.GetInt(), GetTracerType());
+	}
+
+	pOperator->DoMuzzleFlash();
+	m_iClip1--;
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+//-----------------------------------------------------------------------------
+void CWeaponSniperRifle::Operator_ForceNPCFire(CBaseCombatCharacter* pOperator, bool bSecondary)
+{
+	// Ensure we have enough rounds in the magazine
+	m_iClip1++;
+
+	Vector vecShootOrigin, vecShootDir;
+	QAngle angShootDir;
+	GetAttachment(LookupAttachment("muzzle"), vecShootOrigin, angShootDir);
+	AngleVectors(angShootDir, &vecShootDir);
+	FireNPCPrimaryAttack(pOperator, vecShootOrigin, vecShootDir);
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+//-----------------------------------------------------------------------------
+void CWeaponSniperRifle::Operator_HandleAnimEvent(animevent_t* pEvent, CBaseCombatCharacter* pOperator)
+{
+	switch (pEvent->event)
+	{
+	case EVENT_WEAPON_SMG1:
+	{
+		Vector vecShootOrigin, vecShootDir;
+		QAngle angDiscard;
+
+		// Support old style attachment point firing
+		if ((pEvent->options == NULL) || (pEvent->options[0] == '\0') || (!pOperator->GetAttachment(pEvent->options, vecShootOrigin, angDiscard)))
+			vecShootOrigin = pOperator->Weapon_ShootPosition();
+
+		CAI_BaseNPC* npc = pOperator->MyNPCPointer();
+		ASSERT(npc != NULL);
+		vecShootDir = npc->GetActualShootTrajectory(vecShootOrigin);
+
+		FireNPCPrimaryAttack(pOperator, vecShootOrigin, vecShootDir);
+	}
+	break;
+
+	default:
+		BaseClass::Operator_HandleAnimEvent(pEvent, pOperator);
+		break;
+	}
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+// Output : Activity
+//-----------------------------------------------------------------------------
+Activity CWeaponSniperRifle::GetPrimaryAttackActivity()
+{
+	if (m_nShotsFired < 2)
+		return ACT_VM_PRIMARYATTACK;
+
+	if (m_nShotsFired < 3)
+		return ACT_VM_RECOIL1;
+
+	if (m_nShotsFired < 4)
+		return ACT_VM_RECOIL2;
+
+	return ACT_VM_RECOIL3;
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+//-----------------------------------------------------------------------------
+bool CWeaponSniperRifle::Reload()
+{
+	bool fRet = DefaultReload(GetMaxClip1(), GetMaxClip2(), ACT_VM_RELOAD);
+	if (fRet)
+		WeaponSound(RELOAD);
+
+	return fRet;
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+//-----------------------------------------------------------------------------
+void CWeaponSniperRifle::AddViewKick()
+{
+#define EASY_DAMPEN			0.5f
+#define MAX_VERTICAL_KICK	1.0f // Degrees
+#define SLIDE_LIMIT			2.0f // Seconds
+
+	// Get the view kick
+	CBasePlayer* pPlayer = ToBasePlayer(GetOwner());
+
+	if (pPlayer == NULL)
+		return;
+
+	DoMachineGunKick(pPlayer, EASY_DAMPEN, MAX_VERTICAL_KICK, m_fFireDuration, SLIDE_LIMIT);
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+//-----------------------------------------------------------------------------
+void CWeaponSniperRifle::SecondaryAttack()
+{
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+// Input  : flDot -
+//          flDist -
+// Output : int
+//-----------------------------------------------------------------------------
+int CWeaponSniperRifle::WeaponRangeAttack2Condition(float flDot, float flDist)
+{
+	return COND_NONE;
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+//-----------------------------------------------------------------------------
+const WeaponProficiencyInfo_t* CWeaponSniperRifle::GetProficiencyValues()
+{
+	static WeaponProficiencyInfo_t proficiencyTable[] =
+	{
+		{ 7.0,		0.75	},
+		{ 5.00,		0.75	},
+		{ 10.0 / 3.0, 0.75	},
+		{ 5.0 / 3.0,	0.75	},
+		{ 1.00,		1.0		},
+	};
+
+	COMPILE_TIME_ASSERT(ARRAYSIZE(proficiencyTable) == WEAPON_PROFICIENCY_PERFECT + 1);
+
+	return proficiencyTable;
+}
